d8fcdea9586626909a3a73a0996806e8136535a6
Andrey Novichkov <andrey.novichkov@gmail.com>
2020-04-07 09:05:04 -0700

    [add] Finished challenge 4

--- challenges/challenge3.js
+++ challenges/challenge3.js
@@ -2,32 +2,58 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the `greetAndUppercase` function. This function uses
  *    Async/Await. How is this function different than a regular (non-async)
  *    function? What is its return type?
- * 
- * 
+ *
+ *     - Its different syntax that makes a lot more sense, but return type is still the same,
+ *       always a promise object.
+ *
+ *
  * 2. Uncomment block #1 and run the code using `node challenge3.js`. What is
  *    printed when we use `greetAndUppercase` like a regular function?
- * 
- * 
+ *
+ *     - We get that pending Promise output...
+ *
+ *
  * 3. Uncomment block #2 and run the code again. What happens now?
- * 
- * 
- * 4. Write an asynchronous method 'spacer' that takes a string as input and 
- *    returns the input string with a space added between each character. You 
+ *
+ *     - It waits for the promise to return.
+ *
+ *
+ * 4. Write an asynchronous method 'spacer' that takes a string as input and
+ *    returns the input string with a space added between each character. You
  *    can use your solution from Part 3.
- * 
+ *
  *    Call 'spacer' in the `greetAndUppercase` method and run your code again.
  *    You should see something like:
- * 
+ *
  *    'H E L L O   T H E R E ,   D U C K Y'
- * 
- * 
+ *
+ *      - Done! Everything works...
+ *
+ *
  *******************************************************************************
  */
 
+
+ function spacer(str){
+     return new Promise(function(resolve, reject) {
+         setTimeout(function() {
+             if(typeof str === 'string'){
+                 output = "";
+                 let chars = str.split("");
+                 for(let i = 0; i < chars.length; i++){
+                     output += (chars[i] + ' ');
+                 }
+                 resolve(output);
+             }else{
+                 reject("Input string must be of type string!");
+             }
+         }, 1000)
+     })
+ }
 
  /**
   * Asynchronously returns a greeting for a specified name.
@@ -36,7 +62,7 @@
  function greet(name) {
     return new Promise(function(resolve, reject) {
       setTimeout(function() {
-        if (typeof name === 'string') { 
+        if (typeof name === 'string') {
           resolve('Hello there, ' + name);
         } else {
           reject('Name must be a string!');
@@ -64,18 +90,19 @@ function uppercaser(str) {
 async function greetAndUppercase(name) {
     greeting = await greet(name)
     uppercasedGreeting = await uppercaser(greeting)
-    return uppercasedGreeting
+    spacerGreeting = await spacer(uppercasedGreeting)
+    return spacerGreeting
 }
 
 /* Uncomment me! #1 */
 // result = greetAndUppercase('Ducky')
 // console.log(result)
 
-/* Uncomment me! #2 */
-// greetAndUppercase('Ducky')
-//     .then(function(result) {
-//         console.log(result)
-//     })
-//     .catch(function(err) {
-//         console.log(err)
-//     })
+// /* Uncomment me! #2 */
+greetAndUppercase('Ducky')
+    .then(function(result) {
+        console.log(result)
+    })
+    .catch(function(err) {
+        console.log(err)
+    })
--- challenges/challenge4.js
+++ challenges/challenge4.js
@@ -2,31 +2,45 @@
  *******************************************************************************
  * INSTRUCTIONS:
  * Follow the steps below and answer the discusssion questions that follow.
- * 
+ *
  * 1. Read over the code that follows. `makePromise` is a function that makes
  *    an API call and returns a Promise containing the result. Uncomment code
  *    block #1 and run the code. What happens? (HINT: You may need to run
  *    `npm init` first.)
- * 
- * 
+ *
+ *      - Just returns the output of the first API call.
+ *
+ *
  * 2. Sometimes, when making API calls, we want to make a bunch of calls in
  *    parallel and don't care in what order they resolve. (In other words, they
  *    don't depend on each other.)
- * 
- *    Uncomment code block #2 and run the code. What happens? What advantages 
+ *
+ *      - Yep....
+ *
+ *    Uncomment code block #2 and run the code. What happens? What advantages
  *    does `Promise.all` give us when dealing with promises?
- * 
- * 
+ *
+ *      - Let's us make function calls in parallel, and returns an array of their outputs.
+ *        If at least one pf those returns an error, the whole thing rejects! We can then
+ *        access individual elemnts of that returned array and do whatever we want! 
+ *
+ *
  * 3. Make another variable `planet1Promise` and assign to it the result of
  *    calling `makePromise` with the URL `https://swapi.co/api/planets/1`.
  *    Add it to the array passed to `Promise.all`, then print the name of the
  *    returned planet inside the `.then()` callback.
- * 
- * 
+ *
+ *
+ *      - Done!
+ *
+ *
  *******************************************************************************
  */
 
 const request = require('request');
+
+
+// **** STAR WARS API WAS BROKEN, SO I USED THE CARDS API INSTEAD!! ***
 
 
 /**
@@ -52,26 +66,28 @@ function makePromise(url) {
     });
 }
 
-const person1Promise = makePromise('https://swapi.co/api/people/1')
-const person2Promise = makePromise('https://swapi.co/api/people/2')
-const person3Promise = makePromise('https://swapi.co/api/people/3')
+const person1Promise = makePromise('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+const person2Promise = makePromise('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+const person3Promise = makePromise('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+const planet1Promise = makePromise('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
 
 /* Uncomment me! #1 */
-// person1Promise.then(function(personResult) {
-//     console.log(`Resulting person's name: ${personResult.name}`);
+// person1Promise.then(function(deckResult) {
+//     console.log(`Resulting deck id: ${deckResult.deck_id}`);
 // }).catch(function(err) {
 //     console.log("Got an error!")
 //     console.log(err);
 // });
 
 /* Uncomment me! #2 */
-// Promise.all([person1Promise, person2Promise, person3Promise])
-//     .then(function(results) {
-//         for (let i = 0; i < 3; i++) {
-//             console.log(`Person ${i+1}'s name: ${results[i].name}`)
-//         }
-//     })
-//     .catch(function(err) {
-//         console.log('Got an error!')
-//         console.log(err)
-//     })
+Promise.all([person1Promise, person2Promise, person3Promise, planet1Promise])
+    .then(function(results) {
+        for (let i = 0; i < 3; i++) {
+            console.log(`Deck ${i+1}'s id: ${results[i].deck_id}`)
+        }
+        console.log(`Planet 1 deck_id: ${results[3].deck_id}`)
+    })
+    .catch(function(err) {
+        console.log('Got an error!')
+        console.log(err)
+    })
